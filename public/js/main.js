11039dbb6e4dcf2708691d4825bb0417cab4b33a
sumeetranu <satwal1994@gmail.com>
2016-10-25 11:18:44 -0400

    Fixed logos, added tabs in workspace, added about info

--- public/js/main.js
+++ public/js/main.js
@@ -13,7 +13,7 @@ var app = angular.module('vaqoApp', [
 /**
  * Configure the Routes
  */
-app.config(['$routeProvider', function ($routeProvider) {
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider) {
   $routeProvider
     // Home
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
@@ -23,7 +23,12 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/testDb", {templateUrl: "partials/testDb.html", controller: "PageCtrl"})
 
     .when("/connectDb", {templateUrl: "partials/connectDb.html", controller: "PageCtrl"})
-    .when("/workspace", {templateUrl: "partials/workspace.html", controller: "PageCtrl"})
+    .when("/workspace", {templateUrl: "partials/workspace.html", controller: "TabsCtrl"})
+    // Workspace tabs
+    .when("/run", {templateUrl: "partials/run.html", controller: "RunCtrl"})
+    .when("/optimize", {templateUrl: "partials/optimize.html", controller: "OptimizeCtrl"})
+    .when("/convert", {templateUrl: "partials/convert.html", controller: "ConvertCtrl"})
+
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
@@ -32,7 +37,7 @@ app.config(['$routeProvider', function ($routeProvider) {
  * Controls all other Pages
  */
 app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-  console.log("Page Controller reporting for duty.");
+  console.log("Page Controller loaded.");
   // Activates the Carousel
   $('.carousel').carousel({
     interval: 5000
@@ -42,4 +47,44 @@ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
   $('.tooltip-social').tooltip({
     selector: "a[data-toggle=tooltip]"
   })
+});
+
+app.controller('TabsCtrl', function ($scope, $location) {
+  console.log('Tabs Controller loaded.');
+  $scope.tabs = [
+    {link:'/run', bootLink:'#run', label:'Run'},
+    {link:'/optimize', bootLink:'#optimize', label:'Optimize'},
+    {link:'/convert', bootLink:'#convert',label:'Convert'}
+  ];
+
+  $scope.selectedTab = $scope.tabs[0];
+
+  $scope.setSelectedTab = function(tab){
+    $scope.selectedTab = tab;
+  }
+
+  $scope.tabClass = function(tab){
+    if($scope.selectedTab == tab){
+      return "active";
+    } else{
+      return "";
+    }
+
+  }
+
+});
+
+app.controller('RunCtrl', function (/* $scope, $location, $http */) {
+  console.log("Run Controller loaded.");
+
+});
+
+app.controller('OptimizeCtrl', function (/* $scope, $location, $http */) {
+  console.log("Optimize Controller loaded.");
+
+});
+
+app.controller('ConvertCtrl', function (/* $scope, $location, $http */) {
+  console.log("Convert Controller loaded.");
+
 });

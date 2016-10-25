047d86040813d03b2e1639a5f4ed2fcf429bf3ad
Sumeet Ranu <satwal1994@gmail.com>
2016-10-25 13:17:23 -0400

    Added/modified files to work on Azure

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
-var port = 3000;
-
+var port = process.env.port || 1337
 /*app.get('/', function(req, res){
 	res.send("Hello " + req.query.name + " from server.js!");
 });*/
 
 app.use(express.static(__dirname + "/public"));
 
-
-app.listen(port);
 console.log('Server running on port', port);
+
+var server = app.listen(port, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+});

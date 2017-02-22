0a15d1ed59afb1cae13bd01429bb7ef592e2efb0
RCM94 <brodercg@mail.uc.edu>
2017-02-21 21:29:02 -0500

    relalg -> sql doneish

--- prototypes/Parsing/ParseAndTree.js
+++ prototypes/Parsing/ParseAndTree.js
@@ -14,6 +14,8 @@ function Node(type, val)
     this.children = [];
 }
 
+//within a binary operation splits the operation into the two sub operations
+// ex Join(pi a,b(R), S) would output ["pi a,b(R)", "S"]
 function splitOnComma(q)
 {
     if (q.split(",").length == 2)
@@ -46,6 +48,7 @@ function splitOnComma(q)
     }
 }
 
+//parses project for turning into the tree
 function parseProject(str)
 {
     var labels = str.substring(0,str.indexOf("(")).trim();
@@ -97,7 +100,7 @@ function parseNJoin(str)
     right = spl[1];
 
 
-    var node = new Node(TypeEnum.Union, "");
+    var node = new Node(TypeEnum.NJoin, "");
     var subTreeLeft = createTree(left);
     var subTreeRight = createTree(right);
 
@@ -311,21 +314,230 @@ function Node(type, val)
 }
 */
 
-function NodeToSQL(node, subqueries)
+function NodeToSQL(node, subqueries, alias, schema)
 {
+    var query = "";
+    if (node.type == TypeEnum.Pi)
+    {
+        var select = "SELECT DISTINCT " + node.value + "\n";
+        if (node.children[0].type == TypeEnum.Sigma)
+        {
+            query = select + subqueries[0];
+        }
+        else if (node.children[0].children.length == 0)
+        {
+            query = select + "FROM " + subqueries[0];
+        }
+        else
+        {
+            query = select + "FROM (" + subqueries[0] + ") AS alias" + alias.toString();
+            alias++;
+        }
+    }
+    else if (node.type == TypeEnum.Sigma)
+    {
+        var conditions = node.value.replace(",", "AND");
+        query = "FROM " + subqueries[0] + "\nWHERE " + conditions;
+        if (node.parent == TypeEnum.Pi)
+        {
+            query = "SELECT DISTINCT *\n" + query;
+        }
+    }
+    else if (node.type == TypeEnum.Rho)
+    {
+        var changeString = node.value.replace(/\s/g,'');
+        
+        //this... might be difficult. 
+        //as inconvenient as it may be, might have to restrict 
+        //this action on data that has not been operated on
+        if (changeString.includes("\u27F5") && node.children[0].type == TypeEnum.Data)
+        {
+            var changes = changeString.split(',');
+            select = "SELECT DISTINCT ";
 
+            for (i = 1; i < schema[node.children[0].value].length; i++)
+            {
+                var column = schema[node.children[0].value][i][0];
+                var colChange = column;
+                for (j = 0; j < changes.length; j++)
+                {
+                    var change = changes[j].split('\u27F5');
+                    
+                    if (column == change[1])
+                    {
+                        schema[node.children[0].value][i][0] = change[0];
+                        colChange = change[1] + " AS " + change[0];
+                        break;
+                    }
+                }
+                if (i == 1)
+                {
+                    select = select + colChange;
+                }
+                else
+                {
+                    select = select + ", " + colChange;
+                }
+            }
+            query = select + "\nFROM " + node.children[0].value;
+        }
+        else if(!changeString.includes("\u27F5"))
+        {
+            if (node.children[0].type == TypeEnum.Data)
+            {
+                if (node.parent == null)
+                {
+                    query = "SELECT DISTINCT *\nFROM " + subqueries[0] + " AS " + changeString;
+                }
+                else
+                {
+                    query = subqueries[0] + " AS " + changeString;
+                }
+                
+            }
+            else
+            {
+                query = "(SELECT DISTINCT *\nFROM" + subqueries[0] + ") AS " + changeString;
+            }
+
+        }
+    }
+    else if (node.type == TypeEnum.NJoin)
+    {
+        if (node.children[0].type != TypeEnum.Data)
+        {
+            subqueries[0] = "(" + subqueries[0] + ") AS alias" + alias.toString();
+            alias++;
+        }
+        if (node.children[1].type != TypeEnum.Data)
+        {
+            subqueries[1] = "(" + subqueries[1] + ") AS alias" + alias.toString();
+            alias++;
+        }
+        query = subqueries[0] + " NATURAL JOIN " + subqueries[1];
+    }
+    else if (node.type == TypeEnum.Union)
+    {
+        if (node.children[0].type == TypeEnum.Data)
+        {
+            subqueries[0] = "SELECT DISTINCT *\nFROM " + subqueries[0];
+        }
+        if (node.children[1].type == TypeEnum.Data)
+        {
+            subqueries[1] = "SELECT DISTINCT *\nFROM " + subqueries[1];
+        }
+        if (node.parent != null)
+        {
+            query = "(" +  subqueries[0] + "\nUNION\n" + subqueries[1] + ") AS alias" + alias.toString();
+            alias++;
+        }
+        else
+        {
+            query = subqueries[0] + "\nUNION\n" + subqueries[1];
+        }
+        
+    }
+    //this... might be difficult. 
+    //as inconvenient as it may be, might have to restrict 
+    //this action on data that has not been operated on
+    //NEVERMIND TODO FIX THIS SHIT.
+    else if (node.type == TypeEnum.Intersect)
+    {
+        if (node.children[0].type == TypeEnum.Data && node.children[1].type == TypeEnum.Data)
+        {
+            var left = subqueries[0].replace(/\s/g,'') ;
+            var right = subqueries[1].replace(/\s/g,'');
+            var select = "SELECT DISTINCT ";
+            var from = "\nFROM " + left + " JOIN " + right;
+            var on = "\nON "
+            if (left.toLowerCase()!= right.toLowerCase())
+            {
+                for (i = 1; i < schema[left].length; i++)
+                {
+                    var column = schema[left][i][0];
+                    if (i == 1)
+                    {
+                        select = select + left + "." + column;
+                        on = on + left + "." + column + " = " + right + "." + column; 
+                    }
+                    else
+                    {
+                        select = select + ", " + left + "." + column;
+                        on = on + " AND " + left + "." + column + " = " + right + "." + column; 
+                    }
+                    
+                }
+            }
+            else 
+            {
+                
+                var aLeft = "alias" + alias.toString();
+                alias++;
+                var aRight = "alias" + alias.toString();
+                alias++;
+                from = "\nFROM " + left + " AS " + aLeft + " JOIN " + right + " AS " + aRight;
+                for (i = 1; i < schema[left].length; i++)
+                {
+                    var column = schema[left][i][0];
+                    if (i == 1)
+                    {
+                        select = select + aLeft  + "." + column;
+                        on = on + aLeft + "." + column + " = " + aRight + "." + column; 
+                    }
+                    else
+                    {
+                        select = select + ", " + aLeft + "." + column;
+                        on = on + " AND " +  aLeft + "." + column + " = " + aRight + "." + column; 
+                    }
+                }
+            }
+            query = select + from + on;
+        }
+        
+    }
+    //this... might be difficult. 
+    //as inconvenient as it may be, might have to restrict 
+    //this action on data that has not been operated on
+    else if (node.type == TypeEnum.Subtraction)
+    {
+        if (node.children[0].type == TypeEnum.Data)
+        {
+            subqueries[0] = "SELECT DISTINCT *\nFROM " + subqueries[0];
+        }
+        if (node.children[1].type == TypeEnum.Data)
+        {
+            subqueries[1] = "SELECT DISTINCT *\nFROM " + subqueries[1];
+        }
+        if (node.parent != null)
+        {
+            query = "(" +  subqueries[0] + "\nUNION\n" + subqueries[1] + ") AS alias" + alias.toString();
+            alias++;
+        }
+        else
+        {
+            query = subqueries[0] + "\nUNION\n" + subqueries[1];
+        }
+        
+    }
+    else if (node.type == TypeEnum.Data)
+    {
+        var query = node.value
+        
+    }
+    return [query, alias, schema];
 }
 
-function TreeToSql(n, query)
+function TreeToSql(node, query, alias, schema)
 {
-    var i = 0;
-    subqueries = [];
-	while(i < n.children.length)
+    var subqueries = [];
+	for (var i = 0; i < node.children.length; i++)
 	{
-        subqueries.push(TreeToSql(n.children[i], query))
+        var outs = TreeToSql(node.children[i], query, alias, schema);
+        alias = outs[1];
+        subqueries.push(outs[0]);
     }
 
-    return NodeToSQL(node, subqueries);
+    return NodeToSQL(node, subqueries, alias, schema);
 }
 
 /*
@@ -346,13 +558,15 @@ njoin           \u2A1D
 Union           \u222A
 Intersection    \u2229
 Subtraction     \u2A2F
+ARROW           \u27F5
 */
 
-//query = "\t \n  \u03C0 a, c (\n    \u03C3 a < 3 (\n        \u2A1D(pi a,b,c (sigma a < 5 (R)),S)\n    )\n)"
+query = "\t \n  \u03C0 a, b (\n    \u03C3 a < 3 (\n       \u222A(pi b,a (sigma a < 5 (R)),S)\n    )\n)"
+//query = "\u03C0 a, d (\u03C1 d<-c (\u2A2F(S, T))\n)"
 
+//query = "\u03C0 b ,d (\u03C3 b != 'a' (\u222A(\u03C0 b, d(S),T))) "
 
-query = "\u03C0 a, d (\u03C1 d<-c (\u2A2F(S, T))\n)"
-
+//query = "\u2229 (S, S)"
 var cind = query.indexOf("--")
 while (cind != -1)
 {
@@ -368,7 +582,7 @@ while (cind != -1)
     cind = query.indexOf("--")
 }
 
-newq = query.replace(/[^\x21-\x7E\u03C0\u03C1\u03C3\u2A1D\u222A\u2229\u2A2F]+/g, ' ');
+newq = query.replace(/[^\x21-\x7E\u03C0\u03C1\u03C3\u2A1D\u222A\u2229\u2A2F\u27F5]+/g, ' ');
 newq = newq.replace(/^\s+|\s+$/g, '').trim();
 var a = [];
 a.push(1);
@@ -377,21 +591,30 @@ a.push(1);
 var tree = createTree(newq);
 var a = TreeToGraphRun(tree);
 
+var schema = {};
+schema["R"] = [5, ["a", "int"],["b", "string"], ["c", "string"]]
+schema["S"] = [5, ["b", "string"], ["d", "int"]]
+schema["T"] = [4, ["b", "string"], ["d", "int"]]
+
+var b = TreeToSql(tree, "", 0, schema);
+prompt("Copy to clipboard: Ctrl+C, Enter", b[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var nodes = new vis.DataSet(a[0]);
 var edges = new vis.DataSet(a[1]);
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // create a network
 var container = document.getElementById('mynetwork');
